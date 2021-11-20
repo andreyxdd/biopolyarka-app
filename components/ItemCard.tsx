c71a100ec0b4a23659e6a49740d03d4029f6c535
Andrey Volkov <andreyxdd@yandex.ru>
2021-11-20 02:44:23 -0700

    Added instagram-like carusel to cards in the collection

--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -10,7 +10,19 @@ import {
 } from "@mui/material";
 import { IRing } from "../@types/generated/contentful";
 import { IItemProps } from "../types";
+import ClientOnlyDiv from "./ClientOnlyDiv";
 
+// import Swiper core and required modules
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 interface IItemCardProps {
   data: IRing;
   setItems: React.Dispatch<React.SetStateAction<Array<IItemProps>>>;
@@ -51,12 +63,26 @@ const ItemCard: React.FC<IItemCardProps> = ({ data, setItems, id }) => {
         backgroundColor: "white",
       }}
     >
-      <CardMedia
-        component="img"
-        alt={`${title} ${material} ${description}`}
-        height="300"
-        image={`https:${cardImage.fields.file.url}`}
-      />
+      <ClientOnlyDiv>
+        <Swiper
+          grabCursor
+          keyboard={{ enabled: true }}
+          pagination={{ clickable: true }}
+          navigation
+        >
+          {[1, 2, 3].map((val, idx) => (
+            <SwiperSlide key={val * idx}>
+              <CardMedia
+                component="img"
+                alt={`${title} ${material} ${description}`}
+                height="300"
+                image={`https:${cardImage.fields.file.url}`}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </ClientOnlyDiv>
+
       <CardContent>
         <Typography variant="h6" component="div" align="center">
           {title}
